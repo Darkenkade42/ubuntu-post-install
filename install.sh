f493637ff23dd84828b933556931ed06cae2fe9b
Oleksandr Pryymakov <oleksandr.pryymakov@nomnio.com>
2019-12-23 00:02:32 +0100

    Add ctrl to caps switch

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
-cd
+cd ~
 sudo sed -e "/$USER.*/d"  -e "/#includedir \/etc\/sudoers.d/i $USER ALL=(ALL) NOPASSWD:ALL" /etc/sudoers
+sudo sed 's/XKBOPTIONS.*/XKBOPTIONS="ctrl:nocaps"/' /etc/default/keyboard
 sudo apt install git
 git clone https://github.com/Darkenkade42/ubuntu-post-install
 cd ubuntu-post-install
