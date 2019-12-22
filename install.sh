5c647f8b2ada7555e10ca5dbe33d4bb38ac2756f
Oleksandr Pryymakov <oleksandr.pryymakov@nomnio.com>
2019-12-23 00:30:24 +0100

    add inplace flag to sed

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 cd ~
-sudo sed -e "/$USER.*/d"  -e "/#includedir \/etc\/sudoers.d/i $USER ALL=(ALL) NOPASSWD:ALL" /etc/sudoers
-sudo sed 's/XKBOPTIONS.*/XKBOPTIONS="ctrl:nocaps"/' /etc/default/keyboard
+sudo sed -i -e "/$USER.*/d"  -e "/#includedir \/etc\/sudoers.d/i $USER ALL=(ALL) NOPASSWD:ALL" /etc/sudoers
+sudo sed -i 's/XKBOPTIONS.*/XKBOPTIONS="ctrl:nocaps"/' /etc/default/keyboard
 sudo apt install git
 git clone https://github.com/Darkenkade42/ubuntu-post-install
 cd ubuntu-post-install
